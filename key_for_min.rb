e97ea311386c2eef26b4f4915a1df1c382bdfb08
james Johnson <2013matthew2013@gmail.com>
2020-01-30 23:58:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,4 +14,12 @@ it "returns the key of the smallest hash value" do
     expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
   end
   
+  it "does not call the `#min` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:min)
+
+    key_for_min_value(hash)
+  end
+  
   
