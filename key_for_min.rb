b0b1975668840ba5085fb79849c7c753df98fa84
james Johnson <2013matthew2013@gmail.com>
2020-01-30 23:50:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def "smallest hash value" do "
+ it "does not call the `#keys` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
 
   
   key_for_min_value(name_hash)
