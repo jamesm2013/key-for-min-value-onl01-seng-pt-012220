4d475d019d78370167eb8b80c0f6c0e6aa7207c1
james Johnson <2013matthew2013@gmail.com>
2020-01-30 23:50:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def "smallest "
+def "smallest hash value" do "
+  
   key_for_min_value(name_hash)
 
 end
